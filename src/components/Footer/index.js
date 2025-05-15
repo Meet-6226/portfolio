1d3470140f256053016e85238f6ef849669acdfd
Meet Alshi <meetalshi@Mac.lan>
2025-05-15 20:51:12 +0530

    edited

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -87,7 +87,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Suraj Sahani</Logo>
+        <Logo>Meet Alshi</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
@@ -102,7 +102,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2025 Suraj Sahani. All rights reserved.
+          &copy; 2025 Meet Alshi. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
